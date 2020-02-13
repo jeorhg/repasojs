41cca796d5b13f5b1ccfed83a4eaacfd76a93f47
jeorhg <39144963+jeorhg@users.noreply.github.com>
2020-02-13 16:21:41 -0500

    update 13 feb

--- js/app.js
+++ js/app.js
@@ -23,14 +23,16 @@ const contenedorApp = document.querySelector('#app');
 //                 '<li> Trabajo: ' + trabajo + '</li>' +
 //             '</ul>';
 
-let html = `
-        <ul>
-            <li>Nombre:   ${nombre}  </li> 
-            <li>Trabajo:   ${trabajo}  </li> 
-        </ul>
-`;
 
-contenedorApp.innerHTML = html;
+// si activo esto no funciona arreglos y .map --->
+// let html = `
+//         <ul>
+//             <li>Nombre:   ${nombre}  </li> 
+//             <li>Trabajo:   ${trabajo}  </li> 
+//         </ul>
+// `;
+
+// contenedorApp.innerHTML = html;
 
 
 // function declaration
@@ -132,3 +134,157 @@ console.log(tarea1);
 console.log(tarea1.mostrarInformacionTarea() );
 console.log(tarea2);
 console.log(tarea2.mostrarInformacionTarea() );
+
+
+//destructuring de objetos
+const aprendiendoJS ={
+    version:{
+        nueva : 'ES6',
+        anterior : 'ES5',
+    },
+    frameworks: ['React', 'VueJs', 'AngularJS']
+}
+
+//destructuring es extraer valores de un objeto 
+
+// console.log(aprendiendoJS);
+
+// //version anterior
+// let version = aprendiendoJS.version.nueva;
+// let framework = aprendiendoJS.frameworks[1];
+
+
+//destructuring forma nueva
+let {anterior} = aprendiendoJS.version;
+console.log(anterior);
+
+// object literal enhancement
+
+const banda = 'Metallica';
+const genero = 'heavy metal';
+const canciones = 'master of puppets';
+
+// forma anterior
+// const Metallica = {
+//     banda : banda,
+//     genero : genero,
+//     canciones : canciones,
+// }
+
+//forma nueva
+
+const Metallica = {banda, genero, canciones};
+
+console.log(Metallica);
+
+
+//metodos o funciones en un objeto 
+
+// const persona = {
+//     // nombre: 'Jorge',
+//     // trabajo: 'Diseñador web',
+//     (property) musicaRock: Boolean
+//     musicaRock : true,
+//     mostrarInformacion() {
+//         console.log(`${this.nombre} es ${this.trabajo} y su edad es ${this.edad}`)
+//     }
+// }
+
+// persona.mostrarInformacion();
+
+
+//arreglos y .map
+
+//const carrito = ['Producto1', 'Producto2', 'Producto3'];
+// carrito = ['Producto1', 'Producto2', 'Producto3'];
+ 
+// //console.log(carrito);
+
+// // const appContenedor = document.querySelector('#app');
+
+// // let html = '';
+// // carrito.forEach(producto => {
+// //     html += `<li>${producto}</li>`;
+// // })
+
+// // appContenedor.innerHTML = html;
+
+// carrito.map(Producto => {
+//     return 'El producto es ' + Producto;
+// } );
+
+
+//keys
+// const persona = {
+//     nombre: 'Jorge',
+//     trabajo: 'Diseñador web',
+//     edad: '39',
+// }
+
+console.log(Object.keys(persona));
+
+
+//spread operator ...
+
+let lenguajes = ['JS', 'PHP', 'Python'];
+let frameworks = ['ReactJs', 'Laravel', 'Django'];
+
+//unir arreglos en uno solo
+
+// forma antigua  let combinacion = lenguajes.concat(frameworks);
+let combinacion = [...lenguajes,...frameworks];
+
+console.log(combinacion);
+
+
+//filter find y reduce
+
+const personas =[
+    {nombre: 'Jorge', edad: 39, aprendiendo: 'JS'},
+    {nombre: 'Pablo', edad: 28, aprendiendo: 'PHP'},
+    {nombre: 'Alejandra', edad: 21, aprendiendo: 'AdobeXD'},
+    {nombre: 'Carla', edad: 30, aprendiendo: 'Python'},
+    {nombre: 'Miguel', edad: 33, aprendiendo: 'ReactJS'},
+]
+
+console.log(personas);
+
+//mayores de 28
+const mayores = personas.filter(persona => {
+    return persona.edad > 28;
+});
+
+//console.log(mayores);
+
+//que aprende alejandra y su edad
+
+const alejandra = personas.find(persona => {
+    return persona.nombre === 'Alejandra';
+});
+
+//console.log('Alejandra esta aprendiendo: ' + alejandra.aprendiendo);
+
+let total = personas.reduce((edadTotal, persona) => {
+    return edadTotal + persona.edad;
+}, 0);
+console.log(total);
+//console.log(total / personas.length);
+
+
+//PROMISES
+
+const aplicarDescuento = new Promise((resolve, reject) => {
+    setTimeout( () => {
+        let descuento = true;
+
+        if(descuento) {
+            resolve('Descuento aplicado');
+        } else {
+            reject('No se pudo aplicar el descuento')
+        }
+        }, 3000);
+    });
+
+    aplicarDescuento.then(resultado => {
+    console.log(resultado);
+});
