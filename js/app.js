870d5832b8ba79db49ff1d7f8f2ec68104c5d8b0
jeorhg <39144963+jeorhg@users.noreply.github.com>
2020-02-13 17:43:40 -0500

    update 13 feb 17:43

--- js/app.js
+++ js/app.js
@@ -288,3 +288,56 @@ const aplicarDescuento = new Promise((resolve, reject) => {
     aplicarDescuento.then(resultado => {
     console.log(resultado);
 });
+
+
+const descargarUsuarios = cantidad => new Promise((resolve, reject) => {
+//pasar la cantidad a la API
+
+const api = `https://randomuser.me/api/?results=${cantidad}&nat=us`;
+
+//llamando ajax
+const xhr = new XMLHttpRequest();
+
+//abrir la conexion
+xhr.open('GET', api, true);
+
+//on load
+xhr.onload = () => {
+    if(xhr.status === 200) {
+        resolve(JSON.parse(xhr.responseText).results );
+    } else {
+        reject(Error(xhr.statusText));
+    }
+}
+
+//opcional on error
+xhr.onerror = (error) => reject(error);
+
+//send
+xhr.send()
+});
+
+descargarUsuarios(20)
+    .then(
+        miembros => imprimirHTML (miembros),
+        error => console.error(
+            new Error('Hubo un error' + error)
+        )
+    )
+
+    function imprimirHTML(usuarios) {
+        let html = '';
+        usuarios.forEach(usuario => {
+            html += `
+                <li>
+                    Nombre: ${usuario.name.first} ${usuario.name.last}
+                    País: ${usuario.nat}
+                    Imagen:
+                        <img src="${usuario.picture.medium}">
+                </li>
+            `;
+        });
+
+        const contenedorApp = document.querySelector('#app');
+        contenedorApp.innerHTML = html;
+    }
